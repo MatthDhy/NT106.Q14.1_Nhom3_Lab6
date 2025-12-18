cf1038aa2e1fac41df6a746d5706b530b186356a
DHuyyy <24520656@gm.uit.edu.vn>
2025-12-18 14:52:41 +0700

    add shared

---
+++ code_Lab6/code_Lab6/Shared.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace code_Lab6
+{
+    public class MenuItem
+    {   
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+
+    public class OrderItem
+    { 
+        public int TableNumber { get; set; }
+        public int MenuId { get; set; }
+        public string MenuName { get; set; }
+        public int Quantity { get; set; }
+        public int UnitPrice { get; set; }
+    }
+
+}
