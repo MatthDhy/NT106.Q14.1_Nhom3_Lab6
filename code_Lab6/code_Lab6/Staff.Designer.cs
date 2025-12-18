d4eea0bec8e9d3f11d123d3af114eb1fb34eeebe
dhuy-uit <cpdhuy@gmail.com>
2025-12-18 14:02:07 +0700

    Staff

--- code_Lab6/code_Lab6/Staff.Designer.cs
+++ code_Lab6/code_Lab6/Staff.Designer.cs
@@ -28,12 +28,236 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dgvOrders = new System.Windows.Forms.DataGridView();
+            this.TableID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DishName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Quantity = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TotalPrice = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnGetOrders = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCharge = new System.Windows.Forms.Button();
+            this.txtTableID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOrders)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvOrders
+            // 
+            this.dgvOrders.AllowUserToAddRows = false;
+            this.dgvOrders.AllowUserToDeleteRows = false;
+            this.dgvOrders.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvOrders.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvOrders.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.TableID,
+            this.DishName,
+            this.Quantity,
+            this.TotalPrice});
+            this.dgvOrders.Location = new System.Drawing.Point(12, 50);
+            this.dgvOrders.Name = "dgvOrders";
+            this.dgvOrders.ReadOnly = true;
+            this.dgvOrders.Size = new System.Drawing.Size(560, 388);
+            this.dgvOrders.TabIndex = 0;
+            // 
+            // TableID
+            // 
+            this.TableID.HeaderText = "Số Bàn";
+            this.TableID.Name = "TableID";
+            this.TableID.ReadOnly = true;
+            // 
+            // DishName
+            // 
+            this.DishName.HeaderText = "Tên Món";
+            this.DishName.Name = "DishName";
+            this.DishName.ReadOnly = true;
+            // 
+            // Quantity
+            // 
+            this.Quantity.HeaderText = "Số Lượng";
+            this.Quantity.Name = "Quantity";
+            this.Quantity.ReadOnly = true;
+            // 
+            // TotalPrice
+            // 
+            this.TotalPrice.HeaderText = "Thành Tiền";
+            this.TotalPrice.Name = "TotalPrice";
+            this.TotalPrice.ReadOnly = true;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(308, 10);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 1;
+            this.btnConnect.Text = "Kết nối";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnGetOrders
+            // 
+            this.btnGetOrders.Location = new System.Drawing.Point(389, 10);
+            this.btnGetOrders.Name = "btnGetOrders";
+            this.btnGetOrders.Size = new System.Drawing.Size(100, 23);
+            this.btnGetOrders.TabIndex = 2;
+            this.btnGetOrders.Text = "Cập nhật DS";
+            this.btnGetOrders.UseVisualStyleBackColor = true;
+            this.btnGetOrders.Click += new System.EventHandler(this.btnGetOrders_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblTotal);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnCharge);
+            this.groupBox1.Controls.Add(this.txtTableID);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(588, 50);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 200);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thanh Toán";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.ForeColor = System.Drawing.Color.Red;
+            this.lblTotal.Location = new System.Drawing.Point(10, 150);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(19, 20);
+            this.lblTotal.TabIndex = 4;
+            this.lblTotal.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Tổng tiền:";
+            // 
+            // btnCharge
+            // 
+            this.btnCharge.Location = new System.Drawing.Point(10, 80);
+            this.btnCharge.Name = "btnCharge";
+            this.btnCharge.Size = new System.Drawing.Size(180, 35);
+            this.btnCharge.TabIndex = 2;
+            this.btnCharge.Text = "Tính tiền (Charge)";
+            this.btnCharge.UseVisualStyleBackColor = true;
+            this.btnCharge.Click += new System.EventHandler(this.btnCharge_Click);
+            // 
+            // txtTableID
+            // 
+            this.txtTableID.Location = new System.Drawing.Point(10, 45);
+            this.txtTableID.Name = "txtTableID";
+            this.txtTableID.Size = new System.Drawing.Size(180, 20);
+            this.txtTableID.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nhập số bàn:";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(500, 15);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(110, 13);
+            this.lblStatus.TabIndex = 4;
+            this.lblStatus.Text = "Trạng thái: Chưa kết nối";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "IP:";
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(39, 12);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(100, 20);
+            this.txtIP.TabIndex = 6;
+            this.txtIP.Text = "127.0.0.1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(150, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Port:";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(185, 12);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(100, 20);
+            this.txtPort.TabIndex = 8;
+            this.txtPort.Text = "8080";
+            // 
+            // Staff
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Staff";
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnGetOrders);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.dgvOrders);
+            this.Name = "Staff";
+            this.Text = "Staff - Thu Ngân";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Staff_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOrders)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvOrders;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnGetOrders;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnCharge;
+        private System.Windows.Forms.TextBox txtTableID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TableID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DishName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Quantity;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TotalPrice;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPort;
     }
 }
--- code_Lab6/code_Lab6/Staff.cs
+++ code_Lab6/code_Lab6/Staff.cs
@@ -1,20 +1,201 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
+using System.IO;
+using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace code_Lab6
 {
     public partial class Staff : Form
     {
+        private TcpClient client;
+        private StreamReader reader;
+        private StreamWriter writer;
+        private Thread receiveThread;
+        private bool isConnected = false;
+
         public Staff()
         {
             InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string ip = txtIP.Text.Trim();
+                int port;
+
+                if (!int.TryParse(txtPort.Text.Trim(), out port))
+                {
+                    MessageBox.Show("Port phải là số nguyên!");
+                    return;
+                }
+
+                client = new TcpClient(ip, port);
+                NetworkStream stream = client.GetStream();
+                reader = new StreamReader(stream);
+                writer = new StreamWriter(stream) { AutoFlush = true };
+
+                isConnected = true;
+                lblStatus.Text = "Trạng thái: Đã kết nối (" + ip + ":" + port + ")";
+
+                btnConnect.Enabled = false;
+                txtIP.ReadOnly = true;
+                txtPort.ReadOnly = true;
+
+                SendMessage("AUTH STAFF");
+
+                receiveThread = new Thread(ReceiveData);
+                receiveThread.IsBackground = true;
+                receiveThread.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi kết nối: " + ex.Message);
+            }
+        }
+
+        private void SendMessage(string message)
+        {
+            if (isConnected && client != null)
+            {
+                try
+                {
+                    writer.WriteLine(message);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Lỗi gửi dữ liệu: " + ex.Message);
+                }
+            }
+        }
+
+        private void ReceiveData()
+        {
+            try
+            {
+                while (isConnected)
+                {
+                    string message = reader.ReadLine();
+                    if (message == null) break;
+                    this.Invoke(new Action(() => ProcessServerMessage(message)));
+                }
+            }
+            catch
+            {
+                if (isConnected) MessageBox.Show("Mất kết nối với Server.");
+                isConnected = false;
+                Invoke(new Action(() => {
+                    lblStatus.Text = "Trạng thái: Mất kết nối";
+                    btnConnect.Enabled = true;
+                    txtIP.ReadOnly = false;
+                    txtPort.ReadOnly = false;
+                }));
+            }
+        }
+
+        private void ProcessServerMessage(string message)
+        {
+            if (message.StartsWith("TABLE_ORDER:"))
+            {
+                string[] parts = message.Substring(12).Split(',');
+                if (parts.Length == 4)
+                {
+                    dgvOrders.Rows.Add(parts[0], parts[1], parts[2], parts[3]);
+                }
+            }
+            else if (IsNumeric(message))
+            {
+                lblTotal.Text = message + " VNĐ";
+                MessageBox.Show($"Thanh toán thành công! Tổng: {message}");
+
+                string tableID = txtTableID.Text.Trim();
+                RemoveOrdersFromGrid(tableID);
+            }
+        }
+
+        private bool IsNumeric(string value)
+        {
+            long n;
+            return long.TryParse(value, out n);
+        }
+
+        private void btnGetOrders_Click(object sender, EventArgs e)
+        {
+            dgvOrders.Rows.Clear();
+            SendMessage("GET_ORDERS");
+        }
+
+        private void btnCharge_Click(object sender, EventArgs e)
+        {
+            string tableID = txtTableID.Text.Trim();
+            if (string.IsNullOrEmpty(tableID))
+            {
+                MessageBox.Show("Vui lòng nhập số bàn!");
+                return;
+            }
+
+            ExportBillToFile(tableID);
+            SendMessage($"PAY {tableID}");
+        }
+
+        private void ExportBillToFile(string tableID)
+        {
+            try
+            {
+                string fileName = $"bill_Ban{tableID}.txt";
+                using (StreamWriter sw = new StreamWriter(fileName))
+                {
+                    sw.WriteLine($"HÓA ĐƠN THANH TOÁN - BÀN {tableID}");
+                    sw.WriteLine($"Thời gian: {DateTime.Now}");
+                    sw.WriteLine("------------------------------------------------");
+                    sw.WriteLine("Món ăn\t\tSL\tThành tiền");
+
+                    long tempTotal = 0;
+                    foreach (DataGridViewRow row in dgvOrders.Rows)
+                    {
+                        if (row.Cells["TableID"].Value?.ToString() == tableID)
+                        {
+                            string dish = row.Cells["DishName"].Value.ToString();
+                            string qty = row.Cells["Quantity"].Value.ToString();
+                            string price = row.Cells["TotalPrice"].Value.ToString();
+
+                            sw.WriteLine($"{dish}\t\t{qty}\t{price}");
+
+                            long p;
+                            if (long.TryParse(price, out p)) tempTotal += p;
+                        }
+                    }
+                    sw.WriteLine("------------------------------------------------");
+                    sw.WriteLine($"Tạm tính (Client): {tempTotal}");
+                }
+                MessageBox.Show($"Đã xuất hóa đơn tại: {Path.GetFullPath(fileName)}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi xuất file: " + ex.Message);
+            }
+        }
+
+        private void RemoveOrdersFromGrid(string tableID)
+        {
+            for (int i = dgvOrders.Rows.Count - 1; i >= 0; i--)
+            {
+                if (dgvOrders.Rows[i].Cells["TableID"].Value?.ToString() == tableID)
+                {
+                    dgvOrders.Rows.RemoveAt(i);
+                }
+            }
+        }
+
+        private void Staff_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SendMessage("QUIT");
+            isConnected = false;
+            if (client != null) client.Close();
         }
     }
 }
