9c5641016aa7a6684097217f9370053f2ff5eb93
HoangPhu <hoangphu140906@gmail.com>
2025-12-18 15:13:18 +0700

    Add start

---
+++ code_Lab6/code_Lab6/Start.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace code_Lab6
+{
+    partial class Start
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnServer = new System.Windows.Forms.Button();
+            this.btnClient = new System.Windows.Forms.Button();
+            this.btnStaff = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnServer
+            // 
+            this.btnServer.Location = new System.Drawing.Point(135, 178);
+            this.btnServer.Name = "btnServer";
+            this.btnServer.Size = new System.Drawing.Size(118, 59);
+            this.btnServer.TabIndex = 0;
+            this.btnServer.Text = "Server";
+            this.btnServer.UseVisualStyleBackColor = true;
+            this.btnServer.Click += new System.EventHandler(this.btnServer_Click);
+            // 
+            // btnClient
+            // 
+            this.btnClient.Location = new System.Drawing.Point(343, 178);
+            this.btnClient.Name = "btnClient";
+            this.btnClient.Size = new System.Drawing.Size(118, 59);
+            this.btnClient.TabIndex = 1;
+            this.btnClient.Text = "Client";
+            this.btnClient.UseVisualStyleBackColor = true;
+            this.btnClient.Click += new System.EventHandler(this.btnClient_Click);
+            // 
+            // btnStaff
+            // 
+            this.btnStaff.Location = new System.Drawing.Point(550, 178);
+            this.btnStaff.Name = "btnStaff";
+            this.btnStaff.Size = new System.Drawing.Size(118, 59);
+            this.btnStaff.TabIndex = 2;
+            this.btnStaff.Text = "Staff";
+            this.btnStaff.UseVisualStyleBackColor = true;
+            this.btnStaff.Click += new System.EventHandler(this.btnStaff_Click);
+            // 
+            // Start
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnStaff);
+            this.Controls.Add(this.btnClient);
+            this.Controls.Add(this.btnServer);
+            this.Name = "Start";
+            this.Text = "Start";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnServer;
+        private System.Windows.Forms.Button btnClient;
+        private System.Windows.Forms.Button btnStaff;
+    }
+}
